bab0001de5f811632a9f8cc835f8faac26cd0d8c
elkemas <61969319+elkemas@users.noreply.github.com>
2020-05-09 21:53:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 
-const options=document.querySelector(".options").children;
-const answerTrackerContainer=document.querySelector(".answers_tracker");
-const totalQuestionSpan=document.querySelector(".totalQuestion");
-const question=document.querySelector(".question");
-const opt1=document.querySelector(".option 1");
-const opt2=document.querySelector(".option 2");
-const opt3=document.querySelector(".option 3");
-let questionIndex=;
-let index=0;
-let myArray=[];
+const options =document.querySelector(".options").children;
+const answerTrackerBox=document.querySelector(".answers-tracker");
+const questionNumberSpan = document.querySelector(".question-num-value");
+const totalQuestionSpan = document.querySelector(".total-questions");
+const correctAnswerSpan = document.querySelector(".correct-answers");
+const allQuestionsSpan = document.querySelector(".allQuestions");
+const percentage = document.querySelector(".percentage");
+const question = document.querySelector(".question");
+const op1 = document.querySelector(".option1");
+const op2 = document.querySelector(".option2");
+const op3 = document.querySelector(".option3");
+const op4 = document.querySelector(".option4");
+let questionIndex;
+let index = 0;
+let myArray = [];
+let anArr = [];
+let score = 0;
 
 
-function next(){
-    button.classList.add('hidden');
-    document.getElementById('submit').classList.remove('hidden')
-}
 
-
-const question=[
+const questions = [
     {
         q: 'How many hours does a Koala sleep in an average day?',
         options: [ '2 hours', '12 hours', '20 hours'],
         answer: 2
+    },
+    {
+        q: 'Who was the first black Female President?',
+        options: ['Ellen Johnson Sirleaf', 'Shirley Anita Chisholm', 'Margaret Tatcher'],
+        answer: 0
     },
     {
         q: 'Who scored the first Premier League goal in 1992?',
@@ -32,86 +39,138 @@ const question=[
         q: 'How many bones do babies have?',
         options: ['206', 'less than 100', '300'],
         answer: 2
-    }
+    },
     {
         q: 'STEM is an acronym for what?',
-        options: ['skin,tongue,eyes,mouth', 'science,technology,engineering,maths', 'science techniques encapsulating maths'],
+        options: ['Skin, tongue, eyes, mouth', 'Science, technology, engineering, maths', 'Science techniques encapsulating maths'],
         answer: 1
     }
-    {
-        q: 'Who was the first black Female President?',
-        options: ['Ellen Johnson Sirleaf', 'Shirley Anita Chisholm', 'Margaret Tatcher'],
-        answer: 0
-    }
-    ]
+]  
 
-    totalQuestionSpan.innerHTML=questions.length
-    function load(){
-        questionNumberSpan.innerHTML=index+1;
-        question.innerHTML=questions[questionIndex].q;
-        opt1.innerHTML=questions[questionIndex].options[0];
-        opt1.innerHTML=questions[questionIndex].options[1];
-        opt1.innerHTML=questions[questionIndex].options[2];
-        index++;
-    }
+totalQuestionSpan.innerHTML=questions.length;
+function load (){
+    questionNumberSpan.innerHTML=index+1
+    question.innerHTML=questions[questionIndex].q;
+    op1.innerHTML=questions[questionIndex].options[0];
+    op2.innerHTML=questions[questionIndex].options[1];
+    op3.innerHTML=questions[questionIndex].options[2];
+    op4.innerHTML=questions[questionIndex].options[3];
+     index++; 
+}
 
-    function Validate(){
-        if(!options[i].classList.contains("disabled")){
-            alert("please select an option")
+function check(element){
+    if(element.id == questions[questionIndex].answer){
+        element.classList.add("correct");
+        updateAnswerChecker("correct")
+        score++;
         }
-        else {
-            randomQuestions();
-        }
-    }
 
-    function next(){
-        Validate();
-    }
-
-    function randomQuestions(){
-        let randomNumber=Math.floor(Math.random()*questions.length);
-        questionIndex=randomNumber;
-        myArray.push(questionIndex)
-        load();
-    }
-
-    function check(element){
-        if(element.id==questions[questionIndex].answer){
-            element.classList.add("correct");
-            updateAnswerTracker("correct");
-        }
-    }
     else{
         element.classList.add("wrong");
-        updateAnswerTracker("wrong");
+        updateAnswerChecker("wrong");
+    }
+     
+    disabledOptions()
+ 
+}
+
+ 
+function disabledOptions(){
+for(let i=0; i<options.length; i++){
+    options[i].classList.add("disabled");
+    if(options[i].id == questions[questionIndex].answer){
+        options[i].classList.add("correct");
+    }
+}
+}
+
+function enableOptions(){
+    for(let i=0; i<options.length; i++){
+        options[i].classList.remove("disabled","correct","wrong");
     }
 
-    function disabledOptions(){
-        for (let i=0; i<options.length; i++){
-            options[i].classList.add("disabled");
-            if(options[i].id==questions[questionIndex].answer){
-                options[i].classList.add("correct");
-            }
-        }
+}
+
+function validate(){
+    if(!options[0].classList.contains("disabled")) { 
+        return;   
+    }
+    else{
+        enableOptions();
+        randomQuestion();
     }
 
-    function enableoptions(){
-        for (let i=0; i<options.length; i++){
-            options[i].classList.remove("disabled", "correct", "wrong");
+
+}
+
+
+function next(){
+    validate();
+}
+
+function randomQuestion(){
+    let randomNumber = Math.floor(Math.random()*questions.length)
+    let aDuplicate=0;
+    if(index==questions.length){
+        endOFQuiz()
+    }
+    else{
+       if(myArray.length>0) {
+           for(let i=0; i<myArray.length; i++){
+                if(myArray[i]==randomNumber){
+                    aDuplicate=1;
+                    break; 
+
+                }
+           }
+           if(aDuplicate==1){
+               randomQuestion();
+           }
+           else{
+            questionIndex = randomNumber;
+            load();
+            anArr.push(questionIndex);
+           }
+
+       }
+       if(myArray.length==0){
+        questionIndex = randomNumber;
+        load();
+        anArr.push(questionIndex);
+       }
+    
+    myArray.push(randomNumber);
+    
+      
+    
+    }   
+}
+
+function answerchecker(){
+    for (let i=0; i<questions.length; i++){
+        const div=document.createElement("div")
+         answerTrackerBox.appendChild(div);
+
     }
 
-    function answerTracker(){
-        for(let i=0; i<questions.length; i++){
-            const div=document.createElement("div")
-            answerTrackerContainer.appendChild(div);
-        }
-    }
+}
 
-    function updateAnswerTracker(className){
-        answerTrackerContainer.children[index-1].classList.add(className);
-    }
+function updateAnswerChecker(classNam){
+ answerTrackerBox.children[index-1].classList.add(classNam)
+}
 
-    window.onload=function(){
-        randomQuestions();
-        answerTracker();
-    }
+function endOFQuiz(){
+    document.querySelector(".quiz-over").classList.add("show");
+    correctAnswerSpan.innerHTML=score;
+    allQuestionsSpan.innerHTML=questions.length;
+    percentage.innerHTML=(score/questions.length)*100 + "%";
+
+} 
+function tryAgain(){
+    window.location.reload();
+}
+
+window.onload = function(){
+ randomQuestion();
+ answerchecker();
+}
